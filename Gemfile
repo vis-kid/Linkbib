ed8c57c44eb31fdc7f5d70d5f909b834fbfe5443
Ed Wassermann <visavis.kid@gmail.com>
2012-03-12 20:31:06 -0700

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.1'
+gem 'sqlite3'
+gem 'jquery-rails'
+gem 'rspec-rails', :group => [:test, :development]
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do
+  gem 'factory_girl_rails'
+  gem 'capybara'
+  gem 'guard-rspec'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linkbib::Application.config.secret_token = '502a7ad512cc5c5e4650f9eff4e402bf72ea24600c868bb8e4b99b832c292ca6306775209e3692902a6195c2ac078293dc4daddb2966fd1e04b4c6c6ac777ed5'
