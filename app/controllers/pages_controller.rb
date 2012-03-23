89c4da06c1b953e563aed95cb414e324f537b5a7
Ed Wassermann <visavis.kid@gmail.com>
2012-03-22 23:14:43 -0700

    Added Instapaper integration, incomplete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,10 @@ class PagesController < ApplicationController
     if user_signed_in?
       @feed = current_user.feed
       @link = current_user.links.build
+      # @instapaper = "http://www.instapaper.com/api/add?username=benjohnstonsf@gmail.com&password=brucelee1&url=#{link.url}"
     end
   end
 end
+
+
+# @instapaper = "http://www.instapaper.com/api/add?username=benjohnstonsf@gmail.com&password=brucelee1&url=#{link.url}"
